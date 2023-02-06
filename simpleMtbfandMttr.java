c58a4018df18a98ace325b429d79daf4e8a153ea
Alessandro de Oliveira <116854036+Alesd-OR@users.noreply.github.com>
2023-02-05 23:02:33 -0300

    Update simpleMtbfandMttr.java

--- simpleMtbfandMttr.java
+++ simpleMtbfandMttr.java
@@ -1,2 +1,18 @@
-import java util Scanner;
+import java.util.Scanner;
+import java.util.Locale;
 
+public class simpleMtbfandMttr {
+
+    public static void main(String[] args) {
+
+        double hour, timeFailures, discounts;
+        int days;
+        String 
+        System.out.println(
+                "Mean time between failures probably has the most intuitive name of all the metrics we've covered so far.\n It means pretty much what you would've guessed by its name: it is the arithmetic mean (average) amount of time between failures of\n a mechanical or electronic system. The metric usually doesn't include repair times. MTBF values are usually used to predict how likely a single item is to fail within a given timeframe.");
+        System.out.println(
+                "Please, write the assembly line, lithography, stamping or anything else that you want to discover the MTBF/MTTR: ");
+
+    }
+
+}
